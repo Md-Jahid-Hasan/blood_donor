943b071bf86ad3579b0a66ae466002e7d42484f1
Md-Jahid-Hasan <jahid15-1905@diu.edu.bd>
2020-07-15 22:02:25 +0600

    Udate Heroku

--- blood_doner/settings.py
+++ blood_doner/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation
